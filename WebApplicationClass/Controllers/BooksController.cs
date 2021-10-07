51723131f82ce190a3c3299de5319b63e1711884
Vladimir Sumilin <vladimirshumilin42@gmail.com>
2021-10-07 21:58:45 +0200

    REST Services

---
+++ WebApplicationClass/Controllers/BooksController.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLibraryNET;
+using Microsoft.AspNetCore.Http;
+using WebApplicationClass.Managers;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApplicationClass.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private BookManager _manager = new BookManager();
+        // GET: api/Books
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/Books/0 or 1 or 2 or 3
+        [HttpGet("{ibs:int}")]
+        public async Task<ActionResult<Book>> GetBook(int ibs)
+        {
+            try
+            {
+                var result = _manager.GetById(ibs);
+
+                if (result is null)
+                {
+                    return NotFound();
+                }
+
+                return result;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, 
+                    "Error retrieving data from the database");
+            }
+        }
+
+        //[HttpPost]
+        //public async Task<ActionResult<Book>> CreateBook(Book book)
+        //{
+        //    try
+        //    {
+        //        if (book is null)
+        //        {
+        //            return BadRequest();
+        //        }
+        //        return new ActionResult<Book>(book);
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return StatusCode(StatusCodes.Status500InternalServerError,
+        //            "Error retrieving data from the database");
+        //    }
+        //}
+
+        [HttpPut("{ibs:int}")]
+        public async Task<ActionResult<Book>> UpdateBook(int ibs, Book book)
+        {
+            try
+            {
+                if (ibs != book.ISBN13)
+                {
+                    return BadRequest("Book ID mismatch");
+                }
+
+                var bookUpdate = _manager.GetById(ibs);
+                if (bookUpdate is null)
+                {
+                    return NotFound($"Book with Id = {ibs} not found");
+                }
+
+                return _manager.UpdateBook(book);
+                return null;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public void Post([FromBody] Book book)
+        {
+            
+
+        }
+
+
+        // DELETE api/<BooksController>/5
+        [HttpPost]
+        public async Task<ActionResult<Book>> CreateBook(Book book)
+        {
+            try
+            {
+                if (book is null)
+                {
+                    return BadRequest();
+                } 
+                return _manager.AddBook(book);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{ibs:int}")]
+        public async Task<ActionResult<Book>> DeleteBook(int ibs)
+        {
+            try
+            {
+                var deleteBook = _manager.GetById(ibs);
+                if (deleteBook is null)
+                {
+                    return NotFound($"Book with Id = {ibs} not found");
+                }
+
+                return _manager.DeleteBook(ibs);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Error retrieving data from the database");
+            }
+        }
+    }
+}
---
+++ WebApplicationClass/Managers/BookManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ClassLibraryNET;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationClass.Managers
+{
+    public class BookManager
+    {
+        private static List<Book> _books = new List<Book>()
+        {
+                new Book("Antic Hay", "Aldous Huxley", 300, 0),
+                new Book("As I Lay Dying", "William Faulkner", 350, 1),
+                new Book("Death Be Not Proud", "John Gunther", 400, 2),
+                new Book("If Not Now, When?", "Primo Levi", 450,3)
+        };
+
+        public List<Book> GetAll()
+        {
+            return new List<Book>(_books);
+        }
+
+        public ActionResult<Book> GetById(int ibs)
+        {
+            Book book = _books.FirstOrDefault(s => s.ISBN13 == ibs);
+            return book;
+        }
+
+        public Book UpdateBook(Book book)
+        {
+            var result = _books.FirstOrDefault(s => s.ISBN13 == book.ISBN13);
+
+            if (result is not null)
+            {
+                result.Title = book.Title;
+                result.Author = book.Author;
+                result.PageNumber = book.PageNumber;
+
+                return new Book();
+            }
+            return null;
+        }
+
+        public Book AddBook(Book boook)
+        {
+            bool valid = true;
+            foreach (Book item in _books)
+            {
+                if (item.ISBN13 == boook.ISBN13)
+                { 
+                    valid = false;
+                    return null;
+                }
+            }
+            if (valid)
+            {
+                Book book = new Book();
+                _books.Add(book);
+                return book;
+            }
+
+            return null;
+
+        }
+
+        public Book DeleteBook(int ibs)
+        {
+            var result = _books.FirstOrDefault(s => s.ISBN13 == ibs);
+            if (result is not null)
+            {
+                _books.Remove(result);
+                return result;
+            }
+
+            return null;
+        }
+
+
+       
+
+
+    }
+}
